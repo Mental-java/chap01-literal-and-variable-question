afaba3d62ab33dc10656edb7f5a8861f77cb44aa
DESKTOP-OMBAS04\anty3 <anty3125@naver.com>
2023-05-02 15:13:58 +0900

    [level 2-2]문제해결
    연산자 문제 해결 완료

--- src/main/java/com/ohgiraffers/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -11,7 +11,12 @@ public class Application2 {
 		 * 총점 : 201
 		 * 평균 : 67
 		 *  */
-		
+		double kor = 80.5;
+		double math = 50.6;
+		double eng = 70.8;
+		int score = (int)(kor + math + eng);
+		System.out.println("총점 : " + score);
+		System.out.println("평균 : " + (score/3));
 	}
 
 }
