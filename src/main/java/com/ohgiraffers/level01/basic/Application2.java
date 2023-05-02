566d9166a6b7fe4eba02e3cc0d710fc3dde79eca
DESKTOP-OMBAS04\anty3 <anty3125@naver.com>
2023-05-02 13:36:03 +0900

    [level 1-2]문제해결
    연산자 문제 해결 완료

--- src/main/java/com/ohgiraffers/level01/basic/Application2.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application2.java
@@ -14,8 +14,8 @@ public class Application2 {
 		 *  */
 		double width = 12.5;
 		double height = 36.4;
-		System.out.println("둘레 : " + (width*2 + height*2));
 		System.out.println("면적 : " + (width * height));
+		System.out.println("둘레 : " + (width*2 + height*2));
 	}
 
 }
